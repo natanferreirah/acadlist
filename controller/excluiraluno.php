6ceff97013f2f405aea6d0da5d943514c2b1793e
natanferreirah <167040989+natanferreirah@users.noreply.github.com>
2025-06-14 00:10:34 -0300

    Delete feito
    O delete de alunos foi concluido

---
+++ controller/excluiraluno.php
@@ -0,0 +1,29 @@
+<?php 
+require_once '../require/conexao.php';
+session_start();
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $id = $_GET['id'];
+    if (!empty($id)) {
+    $stmt = $conexao->prepare("DELETE FROM alunos WHERE id_aluno = :id_aluno");
+    $stmt->bindValue(':id_aluno', $id);
+        if ($stmt->execute()) {
+            $_SESSION['sucesso_excluir'] = "<p style='color: green; font-weight:600; text-align: center;'>Aluno excluído com sucesso!</p>";
+            header("location: ../alunocrud.php");
+            exit();
+        } else {
+            $_SESSION['erro_excluir'] = "<p style='color: red; font-weight:600; text-align: center;'>Erro ao excluir aluno.</p>";
+            header("location: ../alunocrud.php");
+            exit();
+        }
+    } else {
+        $_SESSION['erro_excluir'] = "<p style='color: red; font-weight:600; text-align: center;'>Erro</p>";
+        header("location: ../alunocrud.php");
+        exit();
+    }
+} else {
+    $_SESSION['erro_excluir'] = "<p style='color: red; font-weight:600; text-align: center;'>ID do aluno não fornecido.</p>";
+    header("location: ../alunocrud.php");
+    exit();
+}
+
+?>
