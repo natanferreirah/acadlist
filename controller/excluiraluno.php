7deebeb120b3c05dabc153b1557b160e11cbf277
natanferreirah <natanferreiradon31@gmail.com>
2025-06-15 15:50:47 -0300

    Corrigindo alguns detalhes

--- controller/excluiraluno.php
+++ controller/excluiraluno.php
@@ -16,12 +16,12 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
             exit();
         }
     } else {
-        $_SESSION['erro_excluir'] = "<p style='color: red; font-weight:600; text-align: center;'>Erro</p>";
+        $_SESSION['erro_excluir'];
         header("location: ../alunocrud.php");
         exit();
     }
 } else {
-    $_SESSION['erro_excluir'] = "<p style='color: red; font-weight:600; text-align: center;'>ID do aluno não fornecido.</p>";
+    $_SESSION['erro_id'] = "<p style='color: red; font-weight:600; text-align: center;'>ID do aluno não fornecido.</p>";
     header("location: ../alunocrud.php");
     exit();
 }
