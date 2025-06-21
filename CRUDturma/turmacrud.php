47be6facf38bc46f881d675d586eed712b5a9573
natanferreirah <natanferreiradon31@gmail.com>
2025-06-21 00:25:21 -0300

    Crud turma organizado em pastas

---
+++ CRUDturma/turmacrud.php
@@ -0,0 +1,104 @@
+<?php
+require_once '../require/conexao.php';
+require_once '../require/protect.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/img/logoicone.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../assets/css/crud.css">
+    <title>Cadastro de Turma - acadlist</title>
+</head>
+
+<body>
+
+    <aside id="menu">
+        <div id="container_logo">
+            <img src="../assets/img/logo2.png" alt="acadlist">
+        </div>
+        <nav id="navbar">
+            <ul>
+                <a href="../painel.php">
+                    <div class="opcao">
+                        <img src="../assets/img/painel.png" alt="" class="icone">
+                        <li>Painel</li>
+                    </div>
+                </a>
+                <a href="../CRUDaluno/alunocrud.php">
+                    <div class="opcao">
+                        <img src="../assets/img/aluno.png" alt="" class="icone">
+                        <li>Cadastra Aluno</li>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="opcao">
+                        <img src="../assets/img/turma.png" alt="" class="icone">
+                        <li>Cadastrar Professor</li>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="opcao">
+                        <img src="../assets/img/materia.png" alt="" class="icone">
+                        <li>Registrar Matéria</li>
+                    </div>
+                </a>
+                <a href="turmacrud.php">
+                    <div class="opcao">
+                        <img src="../assets/img/turma.png" alt="" class="icone">
+                        <li>Registrar Turma</li>
+                    </div>
+                </a>
+            </ul>
+            <form action="../acadlist/require/logout.php " method="post" id="logout">
+                <input type="submit" value="Sair" class="botao">
+            </form>
+        </nav>
+    </aside>
+<?php
+    if (isset($_SESSION['sucesso'])) {
+        echo $_SESSION['sucesso'];
+        unset($_SESSION['sucesso']);
+    }
+    if (isset($_SESSION['erro'])) {
+        echo $_SESSION['erro'];
+        unset($_SESSION['erro']);
+    }
+?>
+    <main id="conteudo">
+        <table border="1px" solid>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Série</th>
+                    <th>Sala Atríbuida</th>
+                    <th>Turno</th>
+                    <th>Ano Letivo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $stmt = $conexao->query("SELECT * FROM turmas");
+                while ($turma = $stmt->fetch()) {
+                    echo "<tr>";
+                    echo "<td>{$turma['id_turma']}</td>";
+                    echo "<td>{$turma['serie']}</td>";
+                    echo "<td>{$turma['sala_atribuida']}</td>";
+                    echo "<td>{$turma['turno']}</td>";
+                    echo "<td>{$turma['ano_letivo']}</td>";
+                    echo '<td><a href="formeditarturma.php?id=' . $turma['id_turma'] . '" class="editar">Editar</a></td>';
+                    echo '<td><a href="excluirturma.php?id=' . $turma['id_turma'] . '" onclick="return confirm(\'Tem certeza que deseja excluir?\')" class="excluir">Excluir</a></td>';
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        <form action="formcadastroturma.php" method="post">
+            <input type="submit" value="Cadastrar" class="botao_cadastrar">
+        </form>
+    </main>
+</body>
+
+</html>
