8e85e61e0f6107e7536e5e9a3268084fdb318687
natanferreirah <natanferreiradon31@gmail.com>
2025-06-21 10:53:01 -0300

    session_start() removido para evitar bugs

--- controller/editarturma.php
+++ controller/editarturma.php
@@ -7,7 +7,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $sala_atribuida = trim($_POST['sala_atribuida']);
         $turno = trim($_POST['turno']);
         $serie = trim($_POST['serie']);
-        echo $serie;
         $ano_letivo = trim($_POST['ano_letivo']);
         if (!empty($id_turma) && !empty($sala_atribuida) && !empty($serie) && !empty($ano_letivo)) {
             $stmt = $conexao->prepare("UPDATE turmas SET sala_atribuida = :sala_atribuida, turno = :turno, serie = :serie, ano_letivo = :ano_letivo WHERE id_turma = :id_turma");
@@ -18,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $stmt->bindValue(':ano_letivo', $ano_letivo);
             if ($stmt->execute()) {
                 $_SESSION['sucesso'] = "<p style='color: #03BBEE; font-weight:600; text-align: center;'>Turma editada com sucesso!</p>";
-                header("location: ../CRUDturma/turmacrud.php?id=$id_turma");
+                header("location: ../CRUDturma/turmacrud.php");
                 exit();
             } else {
                 $_SESSION['erro'] = "<p style='color: red; font-weight:600; text-align: center;'>Erro ao atualizar os dados.</p>";
@@ -27,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         } else {
             $_SESSION['erro'] = "<p style='color: red; font-weight:600; text-align: center;'>Preencha todos os campos obrigatórios.</p>";
-            header("Location: ../CRUDturma/turmacrud.php?id=$id_turma");
+            header("Location: ../CRUDturma/formeditarturma.php?id=$id_turma");
             exit();
         }
     } else {
