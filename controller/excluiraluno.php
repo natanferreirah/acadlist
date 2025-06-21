71b4683c9151b247b55e074e1cab75f121a32734
natanferreirah <natanferreiradon31@gmail.com>
2025-06-21 00:26:48 -0300

    Lógica corrigida e estilizações de erros

--- controller/excluiraluno.php
+++ controller/excluiraluno.php
@@ -4,26 +4,20 @@ require_once '../require/protect.php';
 
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $id = $_GET['id'];
-    if (!empty($id)) {
     $stmt = $conexao->prepare("DELETE FROM alunos WHERE id_aluno = :id_aluno");
     $stmt->bindValue(':id_aluno', $id);
         if ($stmt->execute()) {
-            $_SESSION['sucesso_excluir'] = "<p style='color: green; font-weight:600; text-align: center;'>Aluno excluído com sucesso!</p>";
-            header("location: ../alunocrud.php");
+            $_SESSION['sucesso'] = "<p style='color: green; font-weight:600; text-align: center;'>Aluno excluído com sucesso!</p>";
+            header("location: ../CRUDaluno/alunocrud.php");
             exit();
         } else {
-            $_SESSION['erro_excluir'] = "<p style='color: red; font-weight:600; text-align: center;'>Erro ao excluir aluno.</p>";
-            header("location: ../alunocrud.php");
+            $_SESSION['erro'] = "<p style='color: red; font-weight:600; text-align: center;'>Erro ao excluir aluno.</p>";
+            header("location: ../CRUDaluno/alunocrud.php");
             exit();
         }
-    } else {
-        $_SESSION['erro_excluir'];
-        header("location: ../alunocrud.php");
-        exit();
-    }
 } else {
-    $_SESSION['erro_id'] = "<p style='color: red; font-weight:600; text-align: center;'>ID do aluno não fornecido.</p>";
-    header("location: ../alunocrud.php");
+    $_SESSION['erro'] = "<p style='color: red; font-weight:600; text-align: center;'>ID do aluno não fornecido.</p>";
+    header("location: ../CRUDaluno/alunocrud.php");
     exit();
 }
 
